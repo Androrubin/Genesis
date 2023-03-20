602560d845e52350c829f74dfcd2d369ab66940c
SHREYA262 <92329310+SHREYA262@users.noreply.github.com>
2023-03-20 19:07:01 +0530

    login

--- app/src/main/java/com/androrubin/genesis/login_and_splash/SplashActivity.kt
+++ app/src/main/java/com/androrubin/genesis/login_and_splash/SplashActivity.kt
@@ -1,14 +1,56 @@
 package com.androrubin.genesis.login_and_splash
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.androrubin.genesis.MainActivity
 import com.androrubin.genesis.R
+import com.androrubin.genesis.ui.home.HomeFragment
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        supportActionBar?.hide()
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
 
+        if (user != null) {
+
+            Handler().postDelayed({
+
+                val user = mAuth.currentUser
+                val name = user?.uid
+
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+
+
+            }, 1500)
+        }else {
+            Handler().postDelayed({
+
+                val intent = Intent(this,LoginActivity::class.java)
+                startActivity(intent)
+
+
+            }, 3500)
+
+        }
     }
+
+//    private fun onBoardingFinished(): Boolean {
+//        val sharedPref = applicationContext.getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+//        return sharedPref.getBoolean("Finished",false)
+//
+//
+//
+//    }
 }
--- app/src/main/java/com/androrubin/genesis/ui/home/HomeFragment.kt
+++ app/src/main/java/com/androrubin/genesis/ui/home/HomeFragment.kt
@@ -1,17 +1,23 @@
 package com.androrubin.genesis.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.androrubin.genesis.R
 import com.androrubin.genesis.databinding.FragmentHomeBinding
+import com.androrubin.genesis.login_and_splash.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private  lateinit var auth : FirebaseAuth
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -32,6 +38,10 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+
+
+
         return root
     }
 
