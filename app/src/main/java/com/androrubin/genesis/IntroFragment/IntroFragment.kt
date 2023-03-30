738440d53ba984c020ee3341caf7958ef0f45996
Apoorv yash <apoorvyash123@gmail.com>
2023-03-30 15:04:44 +0530

    added yogasan

---
+++ app/src/main/java/com/androrubin/genesis/IntroFragment/IntroFragment.kt
@@ -0,0 +1,181 @@
+package com.apoorv.runanddetect.IntroFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.textview.MaterialTextView
+import com.google.android.material.transition.MaterialElevationScale
+import com.google.firebase.auth.FirebaseAuth
+import com.apoorv.runanddetect.R
+import com.apoorv.runanddetect.databinding.FragmentIntroBinding
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class IntroFragment : Fragment() , EasyPermissions.PermissionCallbacks{
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var _binding : FragmentIntroBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var materialAlertDialogBuilder: MaterialAlertDialogBuilder
+    private lateinit var customAlertDialogView : View
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentIntroBinding.inflate(inflater, container, false)
+        // your code goes here
+        auth = FirebaseAuth.getInstance()
+
+        enterTransition = MaterialElevationScale(true)
+        exitTransition = MaterialElevationScale(true)
+        reenterTransition = MaterialElevationScale(true)
+
+
+        // material dialog
+        materialAlertDialogBuilder = MaterialAlertDialogBuilder(requireContext())
+
+        // showing the dialog
+        binding.accountIcon.setOnClickListener{
+            customAlertDialogView = LayoutInflater.from(context)
+                .inflate(R.layout.account_dialog_layout, null, false)
+
+            openDialog()
+        }
+
+        binding.openCameraBtn.setOnClickListener{
+            if(!hasCameraPermission()) requestCameraPermission()
+            else{
+                /**
+                 * Opening the camera activity
+                 */
+                findNavController().navigate(R.id.action_introFragment_to_camera)
+
+            }
+        }
+
+
+
+
+        return binding.root
+    }
+
+    private fun openDialog() {
+        materialAlertDialogBuilder.setView(customAlertDialogView)
+
+        val background = materialAlertDialogBuilder.background as MaterialShapeDrawable
+
+        background.shapeAppearanceModel =
+            background.shapeAppearanceModel.toBuilder()
+                .setAllCorners(CornerFamily.ROUNDED, 24.toFloat())
+                .build()
+
+        val alertDialog = materialAlertDialogBuilder.show()
+
+
+        val emailText = customAlertDialogView.findViewById<MaterialTextView>(R.id.email_address_text_view)
+        val signOutBtn = customAlertDialogView.findViewById<MaterialButton>(R.id.sign_out_btn)
+        val guideBtn = customAlertDialogView.findViewById<MaterialButton>(R.id.guideline_btn)
+
+        emailText.text = auth.currentUser.email.toString()
+
+        guideBtn.setOnClickListener{
+            findNavController().navigate(R.id.action_introFragment_to_guideFragment)
+            alertDialog.dismiss()
+        }
+
+        signOutBtn.setOnClickListener{
+            auth.signOut()
+            findNavController().navigate(R.id.action_introFragment_to_loginFragment)
+            alertDialog.dismiss()
+        }
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun hasCameraPermission() =
+        EasyPermissions.hasPermissions(
+            requireContext(),
+            android.Manifest.permission.CAMERA
+        )
+
+    private fun requestCameraPermission(){
+        EasyPermissions.requestPermissions(
+            this,
+            "Camera will be used by this application to detect your running pose",
+            PERMISSION_REQUEST_FOR_CAMERA,
+            android.Manifest.permission.CAMERA
+
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        Toast.makeText(
+            requireContext(),
+            "Permission Granted",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if(EasyPermissions.somePermissionPermanentlyDenied(this, perms)){
+            AppSettingsDialog.Builder(requireActivity()).build().show()
+        }else{
+            requestCameraPermission()
+        }
+    }
+
+    companion object {
+
+        const val PERMISSION_REQUEST_FOR_CAMERA = 1
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            IntroFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ posturedetect/src/main/java/com/apoorv/runanddetect/Authentication/LoginFragment.kt
@@ -0,0 +1,143 @@
+package com.apoorv.runanddetect.Authentication
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.transition.MaterialElevationScale
+import com.google.firebase.auth.FirebaseAuth
+import com.apoorv.runanddetect.R
+import com.apoorv.runanddetect.databinding.FragmentLoginBinding
+
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class LoginFragment : Fragment() {
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var _binding : FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        // your code goes here
+
+        enterTransition = MaterialElevationScale(true)
+        exitTransition = MaterialElevationScale(true)
+        reenterTransition = MaterialElevationScale(true)
+
+
+        // initialize firebase auth
+        auth = FirebaseAuth.getInstance()
+
+        binding.run {
+
+            progressIndicator.hide()
+
+            emailAddressEditText.addTextChangedListener(textWatcher)
+            passwordEditText.addTextChangedListener(textWatcher)
+
+            // navigate to sign up fragment if not registered
+            navToSignUpBtn.setOnClickListener{
+                findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+            }
+
+
+            signInBtn.setOnClickListener{
+                // show progress bar
+                progressIndicator.show()
+                // authentication (sign in)
+                auth.signInWithEmailAndPassword(emailAddressEditText.text.toString().trim(), passwordEditText.text.toString().trim())
+                    .addOnCompleteListener(requireActivity()){
+                        progressIndicator.hide()
+                        if(it.isSuccessful){
+                            val user = auth.currentUser
+                            findNavController().navigate(R.id.action_loginFragment_to_introFragment)
+                        }else{
+                            Toast.makeText(context, "Sign in failed Please try again", Toast.LENGTH_SHORT).show()
+                            passwordEditText.setText("")
+                        }
+                    }
+
+
+            }
+
+
+        }
+
+
+        return binding.root
+    }
+
+    private val textWatcher = object : TextWatcher{
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            binding.run {
+                var emailInput = emailAddressEditText.text.toString().trim()
+                var passwordInput = passwordEditText.text.toString().trim()
+
+                if(passwordEditText.text.toString().trim().length < 3){
+                    passwordInputLayout.error = "Password is too small"
+                }else passwordInputLayout.error = null
+
+                if(!isEmailValid(emailAddressEditText.text.toString().trim())){
+                    emailAddressInputLayout.error = "Invalid email address"
+                }else emailAddressInputLayout.error = null
+
+                // enabling or disabling the button for sign in
+                signInBtn.isEnabled = emailInput.isNotEmpty() && passwordInput.isNotEmpty()
+            }
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+
+        }
+
+    }
+
+    fun isEmailValid(emailInput : String): Boolean{
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+                LoginFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
+                }
+    }
+}
